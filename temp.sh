a1937fb7558904427a8c12f73be99f8187fedb0f
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-04-17 16:55:20 +0700

    .

---
+++ temp.sh
@@ -0,0 +1,13 @@
+# Install flutter
+
+sudo snap install flutter --classic
+flutter sdk-path
+install-android-studio
+# open android studio, choose Standard setup, then waiting Android Studio seting up Android SDK
+# open android studio, choose Tools -> SDK Manager
+#   In System Settings -> Android SDK, tab SDK Tools, 
+#      tick to Android SDK Command Line Tools (latest), choose Apply
+flutter doctor --android-licenses
+flutter doctor
+flutter upgrade
+# open android studio, choose Tools -> Device Manager, Create Device..
