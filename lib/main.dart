c55056f2365381feba2d6d8053614e955bbea8d0
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-04-18 17:21:09 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,10 @@ class MyApp extends StatelessWidget {
         title: 'My App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 237, 190, 180)),
         ),
         home: MyHomePage(),
-      ),
+      ),       
     );
   }
 }
@@ -39,13 +39,15 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;                 // ← Add this.
 
     return Scaffold(
       body: Column(
         children: [
           Text('\n\n\n\n'),
           Text('A random AWSOME idea:'),
-          Text(appState.current.asPascalCase),
+          // Text(appState.current.asPascalCase),
+          BigCard(pair: pair),                // ← Change to this.
           // ↓ Add this.
           ElevatedButton(
             onPressed: () {
@@ -55,6 +57,33 @@ class MyHomePage extends StatelessWidget {
             child: Text('Next'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);       // ← Add this.
+    // ↓ Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,    // ← And also this.
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asPascalCase, style: style),
       ),
     );
   }
