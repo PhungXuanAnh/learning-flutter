339cbe99f4c24b8a5098b13a88449071e45934f0
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-04-24 10:04:31 +0700

    add sample menu bả

---
+++ lib/main.menu_bar.dart
@@ -0,0 +1,35 @@
+// Reference: https://maffan.medium.com/how-to-create-a-side-menu-in-flutter-a2df7833fdfb
+
+import 'package:flutter/material.dart';
+
+import 'widgets/nav_drawer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text('Side menu'),
+      ),
+      body: Center(
+        child: Text('Side Menu Tutorial'),
+      ),
+    );
+  }
+}
